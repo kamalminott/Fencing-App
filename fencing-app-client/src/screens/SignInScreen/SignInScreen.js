840a98f0aa13a7c40aa69f52158d4f8e6ca46af5
Kamal Minott <kamalminott@gmail.com>
2023-06-21 13:30:34 +0100

    added a sign in page with user inputs and sign in button

---
+++ fencing-app-client/src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {Text, StyleSheet, Image, useWindowDimensions, View, ScrollView } from "react-native";
+import Logo from '../../../assets/Logo1.png';
+import CustomInput from "../../components/CustomInput/CustomInput";
+import CustomButton from "../../components/CustomButton/CustomButton"
+
+const SignInScreen = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {height} = useWindowDimensions();
+
+    const onSignInPressed = () => {
+        console.warn('Sign In')
+    }
+
+    const onForgotPasswordPressed = () => {
+        console.warn('Forgot Password')
+    }
+
+    const onSignInFacebook = () => {
+        console.warn('Facebook')
+    }
+
+    const onSignInGoogle = () => {
+        console.warn('Google')
+    }
+
+    const onSignInApple = () => {
+        console.warn('Apple')
+    }
+
+    const onSignUpPressed = () => {
+        console.warn ('On sign up screen')
+    }
+    return(
+        <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.root}>
+            <Image 
+                        source={Logo} 
+                        style={[styles.logo, {height: height * 0.3}]} 
+                        resizeMode="contain"
+            />
+            <CustomInput 
+                        placeholder={"username"} 
+                        value={username} 
+                        setValue={setUsername}
+                        />
+            <CustomInput 
+                        placeholder={"password"} 
+                        value={password} 
+                        setValue={setPassword} 
+                        secureTextEntry
+                        /> 
+            <CustomButton 
+                        text={'Sign In'}
+                        onPress={onSignInPressed}
+            />
+            <CustomButton 
+                        text={'Forgot Password?'} 
+                        onPress={onForgotPasswordPressed}
+                        type="TERTIARY"
+                        />
+            <CustomButton 
+                        text={'Sign In with Facebook'}
+                        onPress={onSignInFacebook}
+                        bgColor={"#E7EAF4"}
+                        fgColor={"#4765A9"}
+            />
+            <CustomButton 
+                        text={'Sign In with Google'}
+                        onPress={onSignInGoogle}
+                        bgColor={"#FAE9EA"}
+                        fgColor={"#DD4D44"}
+            />
+              <CustomButton 
+                        text={'Sign In with Apple'}
+                        onPress={onSignInApple}
+                        bgColor={"#e3e3e3"}
+                        fgColor={"#363636"}
+            />          
+            <CustomButton 
+                        text={"Don't have an account? Create one"} 
+                        onPress={onSignUpPressed}
+                        type="TERTIARY"
+                        />
+        </View>
+    </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    root:{
+        alignItems: 'center',
+        padding: 20,
+    },
+
+    logo:{
+        width: '70%',
+        maxHeight: 300,
+        height: 100,
+    },
+})
+export default SignInScreen
