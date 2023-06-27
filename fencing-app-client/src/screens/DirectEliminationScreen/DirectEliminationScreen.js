d75a6a1307e73ee2640caffa531766360e5a6151
Bernard Simpson <bsimpson8911@gmail.com>
2023-06-27 12:23:07 +0100

    added 3 new pages to app

---
+++ fencing-app-client/src/screens/DirectEliminationScreen/DirectEliminationScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const DirectEliminationScreen = () => {
+  return (
+    <View>
+      <Text>Direct Elimination Screen</Text>
+    </View>
+  );
+};
+
+export default DirectEliminationScreen;
